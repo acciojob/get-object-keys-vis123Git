f7041fe8b1ffcb538fb4989a2f2ca3b4ef1ba2b6
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2023-11-22 21:21:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+const student = {
+  name: 'John Doe'
+};
+
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
+// Test the getKeys() method
+const keys = student.getKeys();
+console.log(keys);
